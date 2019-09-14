94609a333b7d647d5c870baa8bb6dd3400585789
Allenxuxu <120582243@qq.com>
2019-09-14 23:14:35 +0800

    add linkList

---
+++ list/link_list.go
@@ -0,0 +1,129 @@
+package list
+
+type Node struct {
+	data interface{}
+	prev *Node
+	next *Node
+}
+
+type LinkList struct {
+	head   Node
+	length int
+}
+
+func (l *LinkList) PushBack(v interface{}) {
+	node := &l.head
+	for node.next != nil {
+		node = node.next
+	}
+
+	data := &Node{data: v}
+	if node != &l.head {
+		data.prev = node
+	}
+	node.next = data
+	l.length++
+}
+
+func (l *LinkList) PushFront(v interface{}) {
+	data := &Node{data: v}
+	data.next = l.head.next
+	if l.head.next != nil {
+		l.head.next.prev = data
+	}
+	l.head.next = data
+
+	l.length++
+}
+
+func (l *LinkList) Insert(i int, v interface{}) bool {
+	if i > l.length || i < 0 {
+		return false
+	}
+
+	node := &l.head
+	for k := 0; k < i; k++ {
+		node = node.next
+	}
+	data := &Node{data: v}
+	if i != 0 {
+		data.prev = node
+	}
+	data.next = node.next
+	node.next.prev = data
+	node.next = data
+	l.length++
+
+	return true
+}
+
+func (l *LinkList) Get(i int) (interface{}, bool) {
+	if i > l.length || i < 0 {
+		return nil, false
+	}
+
+	node := l.head.next
+	for k := 0; k < i; k++ {
+		node = node.next
+	}
+
+	return node.data, true
+}
+
+func (l *LinkList) PopBack() (interface{}, bool) {
+	if l.length == 0 {
+		return nil, false
+	}
+
+	node := l.head.next
+	for node.next != nil {
+		node = node.next
+	}
+	if node.prev == nil {
+		l.head.next = nil
+	} else {
+		node.prev.next = nil
+		node.prev = nil
+	}
+
+	l.length--
+
+	return node.data, true
+}
+
+func (l *LinkList) PopFront() (interface{}, bool) {
+	if l.length == 0 {
+		return nil, false
+	}
+
+	node := l.head.next
+	if node.next != nil {
+		node.next.prev = nil
+	}
+	l.head.next = node.next
+	l.length--
+
+	return node.data, true
+}
+
+func (l *LinkList) Find(v interface{}) (int, bool) {
+	if l.length == 0 {
+		return -1, false
+	}
+
+	node := l.head.next
+	var index int
+	for node != nil {
+		if node.data == v {
+			return index, true
+		}
+		node = node.next
+		index++
+	}
+
+	return -1, false
+}
+
+func (l *LinkList) Len() int {
+	return l.length
+}
---
+++ list/link_list_test.go
@@ -0,0 +1,100 @@
+package list
+
+import "testing"
+
+func TestLinkList(t *testing.T) {
+	linkList := LinkList{}
+
+	{
+		for i := 0; i < 10; i++ {
+			linkList.PushBack(i)
+		}
+
+		for i := 9; i >= 0; i-- {
+			v, ok := linkList.Get(i)
+			if ok != true {
+				t.Fatal()
+			}
+			if v.(int) != i {
+				t.Fatal()
+			}
+			v, ok = linkList.Find(i)
+			if ok != true {
+				t.Fatal()
+			}
+			if v != i {
+				t.Fatal()
+			}
+			v, ok = linkList.PopBack()
+			if ok != true {
+				t.Fatal()
+			}
+			if v.(int) != i {
+				t.Fatal(i, v)
+			}
+		}
+
+		_, ok := linkList.PopBack()
+		if ok != false {
+			t.Fatal()
+		}
+
+		if linkList.Len() != 0 {
+			t.Fatal()
+		}
+	}
+
+	{
+		linkList.PushFront(0)
+		v, ok := linkList.Get(0)
+		if ok != true {
+			t.Fatal()
+		}
+		if v.(int) != 0 {
+			t.Fatal()
+		}
+
+		linkList.PushFront(-1)
+		linkList.PushFront(-2)
+		linkList.PushFront(-3)
+		v, ok = linkList.PopFront()
+		if ok != true {
+			t.Fatal()
+		}
+		if v.(int) != -3 {
+			t.Fatal()
+		}
+		v, ok = linkList.PopBack()
+		if ok != true {
+			t.Fatal()
+		}
+		if v.(int) != 0 {
+			t.Fatal(v)
+		}
+		v, ok = linkList.Get(1)
+		if ok != true {
+			t.Fatal()
+		}
+		if v.(int) != -1 {
+			t.Fatal()
+		}
+		v, ok = linkList.PopFront()
+		if ok != true {
+			t.Fatal()
+		}
+		if v.(int) != -2 {
+			t.Fatal(v)
+		}
+		v, ok = linkList.PopFront()
+		if ok != true {
+			t.Fatal()
+		}
+		if v.(int) != -1 {
+			t.Fatal()
+		}
+
+		if linkList.Len() != 0 {
+			t.Fatal()
+		}
+	}
+}
