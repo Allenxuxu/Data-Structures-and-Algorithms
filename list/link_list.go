f6dad37b8c8d3938bc4ed5f5ffa4c7611c82b513
Allenxuxu <120582243@qq.com>
2019-09-15 16:05:01 +0800

    update link list

--- list/link_list.go
+++ list/link_list.go
@@ -7,67 +7,39 @@ type Node struct {
 }
 
 type LinkList struct {
-	head   Node
+	head   *Node
+	tail   *Node
 	length int
 }
 
 func (l *LinkList) PushBack(v interface{}) {
-	node := &l.head
-	for node.next != nil {
-		node = node.next
+	data := &Node{data: v}
+
+	if l.head == nil {
+		l.head = data
+		l.tail = data
+	} else {
+		l.tail.next = data
+		data.prev = l.tail
+		l.tail = data
 	}
 
-	data := &Node{data: v}
-	if node != &l.head {
-		data.prev = node
-	}
-	node.next = data
 	l.length++
 }
 
 func (l *LinkList) PushFront(v interface{}) {
 	data := &Node{data: v}
-	data.next = l.head.next
-	if l.head.next != nil {
-		l.head.next.prev = data
+
+	if l.head == nil {
+		l.head = data
+		l.tail = data
+	} else {
+		l.head.prev = data
+		data.next = l.head
+		l.head = data
 	}
-	l.head.next = data
 
 	l.length++
-}
-
-func (l *LinkList) Insert(i int, v interface{}) bool {
-	if i > l.length || i < 0 {
-		return false
-	}
-
-	node := &l.head
-	for k := 0; k < i; k++ {
-		node = node.next
-	}
-	data := &Node{data: v}
-	if i != 0 {
-		data.prev = node
-	}
-	data.next = node.next
-	node.next.prev = data
-	node.next = data
-	l.length++
-
-	return true
-}
-
-func (l *LinkList) Get(i int) (interface{}, bool) {
-	if i >= l.length || i < 0 {
-		return nil, false
-	}
-
-	node := l.head.next
-	for k := 0; k < i; k++ {
-		node = node.next
-	}
-
-	return node.data, true
 }
 
 func (l *LinkList) PopBack() (interface{}, bool) {
@@ -75,19 +47,18 @@ func (l *LinkList) PopBack() (interface{}, bool) {
 		return nil, false
 	}
 
-	node := l.head.next
-	for node.next != nil {
-		node = node.next
-	}
-	if node.prev == nil {
-		l.head.next = nil
-	} else {
+	node := l.tail
+	l.tail = node.prev
+	if node.prev != nil {
 		node.prev.next = nil
-		node.prev = nil
+	}
+	node.prev = nil
+
+	if l.tail == nil {
+		l.head = nil
 	}
 
 	l.length--
-
 	return node.data, true
 }
 
@@ -96,13 +67,18 @@ func (l *LinkList) PopFront() (interface{}, bool) {
 		return nil, false
 	}
 
-	node := l.head.next
+	node := l.head
+	l.head = node.next
 	if node.next != nil {
 		node.next.prev = nil
 	}
-	l.head.next = node.next
-	l.length--
+	node.next = nil
 
+	if l.head == nil {
+		l.tail = nil
+	}
+
+	l.length--
 	return node.data, true
 }
 
@@ -111,7 +87,7 @@ func (l *LinkList) Find(v interface{}) (int, bool) {
 		return -1, false
 	}
 
-	node := l.head.next
+	node := l.head
 	var index int
 	for node != nil {
 		if node.data == v {
@@ -124,11 +100,71 @@ func (l *LinkList) Find(v interface{}) (int, bool) {
 	return -1, false
 }
 
+func (l *LinkList) Insert(i int, v interface{}) bool {
+	if i > l.length || i < 0 {
+		return false
+	}
+
+	if i == 0 {
+		l.PushFront(v)
+		return true
+	}
+	if i == l.length {
+		l.PushBack(v)
+		return true
+	}
+
+	data := &Node{data: v}
+	var node *Node
+	if i < (l.length / 2) {
+		node = l.head
+		for k := 0; k < i; k++ {
+			node = node.next
+		}
+	} else {
+		node = l.tail
+		index := l.length - 1 - i
+		for k := 0; k < index; k++ {
+			node = node.prev
+		}
+	}
+	node.prev.next = data
+	data.prev = node.prev
+	node.next.prev = data
+	data.next = node.next
+
+	l.length++
+	return true
+}
+
+func (l *LinkList) Get(i int) (interface{}, bool) {
+	if i >= l.length || i < 0 {
+		return nil, false
+	}
+
+	var node *Node
+	if i < (l.length / 2) {
+		node = l.head
+		for k := 0; k < i; k++ {
+			node = node.next
+		}
+	} else {
+		node = l.tail
+		index := l.length - 1 - i
+		for k := 0; k < index; k++ {
+			node = node.prev
+		}
+	}
+
+	return node.data, true
+}
+
 func (l *LinkList) Length() int {
 	return l.length
 }
 
 func (l *LinkList) Clear() {
-	l.head.next = nil
+	l.head = nil
+	l.tail = nil
 	l.length = 0
 }
